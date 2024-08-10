893f2264636e2924f3d8682fa1df4b68e8a312fb
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2024-08-10 05:01:09 -0500

    Add search for endpoint operations

--- Needlework.Net.Desktop/ViewModels/EndpointViewModel.cs
+++ Needlework.Net.Desktop/ViewModels/EndpointViewModel.cs
@@ -12,8 +12,12 @@ namespace Needlework.Net.Desktop.ViewModels
         public string Endpoint { get; }
         public string Title => Endpoint;
 
+
         [ObservableProperty] private IAvaloniaReadOnlyList<PathOperationViewModel> _pathOperations;
         [ObservableProperty] private PathOperationViewModel? _selectedPathOperation;
+
+        [ObservableProperty] private string? _search;
+        [ObservableProperty] private IAvaloniaReadOnlyList<PathOperationViewModel> _filteredPathOperations;
 
         public EndpointViewModel(string endpoint)
         {
@@ -21,6 +25,18 @@ namespace Needlework.Net.Desktop.ViewModels
 
             var handler = WeakReferenceMessenger.Default.Send<DataRequestMessage>().Response;
             PathOperations = new AvaloniaList<PathOperationViewModel>(handler.Plugins[endpoint].Select(x => new PathOperationViewModel(x)));
+            FilteredPathOperations = new AvaloniaList<PathOperationViewModel>(PathOperations);
+        }
+
+        partial void OnSearchChanged(string? value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                FilteredPathOperations = new AvaloniaList<PathOperationViewModel>(PathOperations);
+                return;
+            }
+
+            FilteredPathOperations = new AvaloniaList<PathOperationViewModel>(PathOperations.Where(o => o.Path.ToLower().Contains(value.ToLower())));
         }
     }
 }
