a24a72b3b24ead70f9ac88e983a32a767164c51c
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2024-08-19 04:33:46 -0500

    Update about page

--- Needlework.Net/ViewModels/AboutViewModel.cs
+++ Needlework.Net/ViewModels/AboutViewModel.cs
@@ -1,9 +1,26 @@
-﻿namespace Needlework.Net.ViewModels
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Diagnostics;
+using System.Net.Http;
+
+namespace Needlework.Net.ViewModels
 {
-    public class AboutViewModel : PageBase
+    public partial class AboutViewModel : PageBase
     {
-        public AboutViewModel() : base("About", "info-circle")
+        public HttpClient HttpClient { get; }
+
+        public AboutViewModel(HttpClient httpClient) : base("About", "info-circle")
         {
+            HttpClient = httpClient;
+        }
+
+        [RelayCommand]
+        private void OpenUrl(string url)
+        {
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo(url) { UseShellExecute = true }
+            };
+            process.Start();
         }
     }
 }
