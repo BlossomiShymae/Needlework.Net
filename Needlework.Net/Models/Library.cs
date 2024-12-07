e9d4615ecf0d6b68177cc1bcbf5a55dc14093036
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2024-12-06 18:59:43 -0600

    Add libraries list

---
+++ Needlework.Net/Models/Library.cs
@@ -0,0 +1,9 @@
+﻿namespace Needlework.Net.Models;
+
+public class Library
+{
+    public required string Repo { get; init; }
+    public string? Description { get; init; }
+    public required string Language { get; init; }
+    public required string Link { get; init; }
+}
--- Needlework.Net/ViewModels/Pages/HomeViewModel.cs
+++ Needlework.Net/ViewModels/Pages/HomeViewModel.cs
@@ -1,10 +1,17 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using Avalonia.Platform;
+using CommunityToolkit.Mvvm.Input;
+using Needlework.Net.Models;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Text.Json;
 
 namespace Needlework.Net.ViewModels.Pages;
 
 public partial class HomeViewModel : PageBase
 {
+    public List<Library> Libraries { get; } = JsonSerializer.Deserialize<List<Library>>(AssetLoader.Open(new Uri($"avares://NeedleworkDotNet/Assets/libraries.json")))!;
+
     public HomeViewModel() : base("Home", "home", int.MinValue) { }
 
     [RelayCommand]
