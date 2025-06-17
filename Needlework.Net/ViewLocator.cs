910d26c00d262867b20e00e8ead11626bf773233
estrogen elf <87099578+BlossomiShymae@users.noreply.github.com>
2025-06-17 11:51:08 -0500

    refactor: remove control cache to fix bugs

--- Needlework.Net/ViewLocator.cs
+++ Needlework.Net/ViewLocator.cs
@@ -1,59 +1,14 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Controls.Templates;
-using Avalonia.VisualTree;
-using BitFaster.Caching;
-using BitFaster.Caching.Lru;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
 
 namespace Needlework.Net
 {
     public class ViewLocator : IDataTemplate
     {
-        private class ObjectComparer : IEqualityComparer<object>
-        {
-            public new bool Equals(object? x, object? y)
-            {
-                if (ReferenceEquals(x, y))
-                    return true;
-
-                if (x == null || y == null)
-                    return false;
-
-                return x.Equals(y);
-            }
-
-            public int GetHashCode([DisallowNull] object obj)
-            {
-                return obj.GetHashCode();
-            }
-        }
-
-        private readonly ICache<object, Control> _controlCache = new ConcurrentLruBuilder<object, Control>()
-            .WithExpireAfterAccess(TimeSpan.FromMinutes(5))
-            .WithKeyComparer(new ObjectComparer())
-            .WithCapacity(1024)
-            .WithMetrics()
-            .Build();
-
         private readonly Dictionary<Type, Func<Control>> _viewRegister = [];
-
-        public ViewLocator()
-        {
-            _controlCache.Events.Value!.ItemRemoved += (source, args) =>
-            {
-                var descendants = args.Value!.GetVisualDescendants();
-                foreach (var descendant in descendants)
-                {
-                    if (descendant.DataContext is INotifyPropertyChanged key)
-                    {
-                        _controlCache.TryRemove(key, out _);
-                    }
-                }
-            };
-        }
 
         public void Register<T>(Func<Control> viewActivator)
             where T : INotifyPropertyChanged
@@ -68,13 +23,7 @@ namespace Needlework.Net
                 throw new Exception("Data type has no registered view activator.");
             }
 
-            bool isCold = !_controlCache.TryGet(data!, out var res);
-            if (isCold)
-            {
-                res ??= activator();
-                _controlCache.AddOrUpdate(data!, res);
-            }
-
+            var res = activator();
             res!.DataContext = data;
             return res;
         }
